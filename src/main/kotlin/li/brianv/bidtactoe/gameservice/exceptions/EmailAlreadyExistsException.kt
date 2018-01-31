be2d87a5da9bb46a9e0607cdde8ef7f7ffc7d6ca
Brian <brnli7@gmail.com>
2018-01-31 12:54:57 -0500

    Updated Exceptions

--- src/main/kotlin/li/brianv/bidtactoe/gameservice/exceptions/EmailAlreadyExistsException.kt
+++ src/main/kotlin/li/brianv/bidtactoe/gameservice/exceptions/EmailAlreadyExistsException.kt
@@ -3,5 +3,5 @@ package li.brianv.bidtactoe.gameservice.exceptions
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Email already in use")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Email already in use")
 class EmailAlreadyExistsException : RuntimeException()
