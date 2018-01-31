9140a09c50c475987cd0b49edbc2a1c30bf669cc
Brian <brnli7@gmail.com>
2018-01-31 12:29:34 -0500

    Updated MongoDB Cloud Client

--- src/main/kotlin/li/brianv/bidtactoe/gameservice/GameServiceApplication.kt
+++ src/main/kotlin/li/brianv/bidtactoe/gameservice/GameServiceApplication.kt
@@ -6,13 +6,16 @@ import li.brianv.bidtactoe.gameservice.game.MoveMaker
 import li.brianv.bidtactoe.gameservice.game.Player
 import org.riversun.fcm.FcmClient
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler
 import java.util.*
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = arrayOf(MongoAutoConfiguration::class))
 class GameServiceApplication {
     @Bean
     fun provideGameManager(moveMaker: MoveMaker, gameFCMComponent: GameFCMComponent): GameManager {
