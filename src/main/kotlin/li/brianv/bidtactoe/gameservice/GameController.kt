37465bdfb023f6ab27a23f797527dcf3f3f8aa8b
home <brnli7@gmail.com>
2018-02-12 13:00:13 -0500

    Fixed CORS problem with web app

--- src/main/kotlin/li/brianv/bidtactoe/gameservice/GameController.kt
+++ src/main/kotlin/li/brianv/bidtactoe/gameservice/GameController.kt
@@ -7,43 +7,41 @@ import li.brianv.bidtactoe.gameservice.repository.UserRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
 class GameController(val gameManager: GameManager, val userRepository: UserRepository) {
 
     val logger: Logger = LoggerFactory.getLogger(GameController::class.java.simpleName)
 
-    @RequestMapping(method = arrayOf(RequestMethod.POST),
-            value = "/join_game",
-            consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    @RequestMapping(method = [(RequestMethod.POST)],
+            value = ["/join_game"],
+            consumes = [(MediaType.APPLICATION_FORM_URLENCODED_VALUE)])
     fun joinGame(playerId: String, deviceId: String) {
         logger.info("Join Game: playerId: $playerId, deviceId: $deviceId")
         gameManager.joinGame(playerId, deviceId)
     }
 
-    @RequestMapping(method = arrayOf(RequestMethod.POST),
-            value = "/leave_queue",
-            consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    @RequestMapping(method = [(RequestMethod.POST)],
+            value = ["/leave_queue"],
+            consumes = [(MediaType.APPLICATION_FORM_URLENCODED_VALUE)])
     fun leaveQueue(playerId: String) {
         logger.info("Leave Queue: playerId: $playerId")
         gameManager.leaveQueue(playerId)
     }
 
-    @RequestMapping(method = arrayOf(RequestMethod.POST),
-            value = "/bid",
-            consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    @RequestMapping(method = [(RequestMethod.POST)],
+            value = ["/bid"],
+            consumes = [(MediaType.APPLICATION_FORM_URLENCODED_VALUE)])
     fun bid(playerId: String, gameIndex: Int, bidAmt: Int) {
         logger.info("Bid: playerId: $playerId, gameIndex: $gameIndex, bidAmt: $bidAmt")
         gameManager.bid(playerId, gameIndex, bidAmt)
     }
 
-    @RequestMapping(method = arrayOf(RequestMethod.POST),
-            value = "/make_move",
-            consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    @RequestMapping(method = [(RequestMethod.POST)],
+            value = ["/make_move"],
+            consumes = [(MediaType.APPLICATION_FORM_URLENCODED_VALUE)])
     fun makeMove(gameIndex: Int, cells: String) {
         logger.info("Make Move: gameIndex $gameIndex, cells: $cells")
         for (cell in cells.convertStringToCellsArray()) {
@@ -52,9 +50,9 @@ class GameController(val gameManager: GameManager, val userRepository: UserRepos
         gameManager.makeMove(gameIndex, cells)
     }
 
-    @RequestMapping(method = arrayOf(RequestMethod.POST),
-            value = "/login",
-            consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    @RequestMapping(method = [(RequestMethod.POST)],
+            value = ["/login"],
+            consumes = [(MediaType.APPLICATION_FORM_URLENCODED_VALUE)])
     @ResponseBody
     fun logIn(email: String, password: String): User {
         logger.info("Log In: email $email, password: $password")
@@ -65,9 +63,9 @@ class GameController(val gameManager: GameManager, val userRepository: UserRepos
             throw BadLoginException()
     }
 
-    @RequestMapping(method = arrayOf(RequestMethod.POST),
-            value = "/signup",
-            consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    @RequestMapping(method = [(RequestMethod.POST)],
+            value = ["/signup"],
+            consumes = [(MediaType.APPLICATION_FORM_URLENCODED_VALUE)])
     @ResponseBody
     fun signUp(username: String, email: String, password: String) {
         logger.info("Sign Up: username: $username, email $email, password: $password")
